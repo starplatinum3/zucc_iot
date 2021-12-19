dfc5ee8eb1b3ae7c869e720938553988e0afdfb9
starplatinumora <2500441778@qq.com>
2021-12-19 15:27:11 +0800

    big work

--- bigWork/emq.cpp
+++ bigWork/emq.cpp
@@ -26,15 +26,23 @@ StaticJsonDocument<200> doc;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 // Setup a feed called 'photocell' for publishing.
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "iot/1");
+Adafruit_MQTT_Publish pubTemp = Adafruit_MQTT_Publish(&mqtt, "ZUCC-MQP/esp");
 
 // Setup a feed called 'onoff' for subscribing to changes.
-// Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "iot/2");
-Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "mqp/rgb");
-
+// Adafruit_MQTT_Subscribe subRgb = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "iot/2");
+Adafruit_MQTT_Subscribe subRgb= Adafruit_MQTT_Subscribe(&mqtt,"ZUCC-MQP/rgb");
+Adafruit_MQTT_Subscribe subOled = Adafruit_MQTT_Subscribe(&mqtt, "ZUCC-MQP/oled");
 // Bug workaround for Arduino 1.6.6, it seems to need a function declaration
 // for some reason (only affects ESP8266, likely an arduino-builder bug).
 void MQTT_connect();
+
+// MQTT topic 命名规则（ZUCC-IOT姓名缩写，比如”ZUCC-IOTCJP”）：
+// ZUCC-CJP/esp：小程序用于接收来自 ESP8266 板的温湿度数据，在小程序端可以
+// 加载 echarts 图表显示温湿度值；
+// ZUCC-CJP/rgb：小程序控制 ESP8266 板上的 RGB 灯的显示，小程序端可以用调
+// 色盘设定 RGB 值，用于控制 ESP8266 板上的 RGB 灯显示；
+// ZUCC-CJP/oled：用于输出字符，显示在 ESP8266 的 OLED 屏上。输出字符必须
+// 包含本人姓名缩写和学号，同时 OLED 屏要显示实时温湿度数值，中文字符显示可选。
 
 const int pin_r = 12;
 const int pin_g = 13;
@@ -85,7 +93,7 @@ void setup()
   Serial.println(WiFi.localIP());
 
   // Setup MQTT subscription for onoff feed.
-  mqtt.subscribe(&onoffbutton);
+  mqtt.subscribe(&subRgb);
 }
 
 int strSame(char *str, char *str2)
@@ -94,6 +102,19 @@ int strSame(char *str, char *str2)
 }
 #include <string.h>
 uint32_t x = 0;
+
+void displayRead(Adafruit_MQTT_Subscribe sub){
+  Serial.print(F("Got: "));
+      char *read = (char *)sub.lastread;
+
+      // Serial.println((char *)subOled.lastread);
+ Serial.println(read);
+      display.flipScreenVertically();
+      display.clear();
+      display.drawString(0, 10, "topic: iot/1");
+      display.drawString(0, 20,read);
+      display.display();
+}
 void loop()
 {
   int chk = DHT11.read(5);
@@ -108,25 +129,25 @@ void loop()
   Adafruit_MQTT_Subscribe *subscription;
   while ((subscription = mqtt.readSubscription(5000)))
   {
-    if (subscription == &onoffbutton)
+    if (subscription == &subRgb)
     {
       Serial.print(F("Got: "));
-      Serial.println((char *)onoffbutton.lastread);
+      Serial.println((char *)subRgb.lastread);
 
-      char *read = (char *)onoffbutton.lastread;
+      char *read = (char *)subRgb.lastread;
 
       display.flipScreenVertically();
       display.clear();
       display.drawString(0, 10, "topic: iot/1");
-      display.drawString(0, 20, (char *)onoffbutton.lastread);
+      display.drawString(0, 20, (char *)subRgb.lastread);
       display.display();
 
-      drawRgb(111, 111, 111);
+      // drawRgb(111, 111, 111);
 
-      //       StaticJsonDocument<200> ledJson;
-      // //   deserializeJson(ledJson,(char*)onoffbutton.lastread);
-      //     deserializeJson(ledJson,read);
-      //   drawRgb(ledJson["r"],ledJson["g"],ledJson["b"]);
+            StaticJsonDocument<200> ledJson;
+      //   deserializeJson(ledJson,(char*)subRgb.lastread);
+          deserializeJson(ledJson,read);
+        drawRgb(ledJson["r"],ledJson["g"],ledJson["b"]);
 
       //        if(strcmp(read,"1")==0){
       // //   drawRgb()
@@ -140,6 +161,9 @@ void loop()
       //  }
 
       delay(2000);
+    }else if(subscription == &subOled){
+      displayRead(subOled);
+
     }
   }
   // Now we can publish stuff!
